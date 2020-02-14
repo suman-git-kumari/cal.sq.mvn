46d9dd75cbdee8b8ddf0d01440486ebf02d294a1
suman.f.kumari <suman.f.kumari@accenture.com>
2020-02-14 12:20:16 +0530

    changing the source code by me

--- src/main/java/calc/calc/sq/mvn/AgeCalc.java
+++ src/main/java/calc/calc/sq/mvn/AgeCalc.java
@@ -1,14 +1,13 @@
 package calc.calc.sq.mvn;
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Arrays;
-import java.io.Externalizable;
+#import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.time.DateTimeException;
 
-public class AgeCalc implements Externalizable {
+public class AgeCalc{
 	private String inputDate;
 	public AgeCalc(String inputDate){
 		this.inputDate = inputDate;
@@ -17,7 +16,7 @@ public class AgeCalc implements Externalizable {
 		AgeCalc obj = new AgeCalc(inputDate);
 		String temp[] = inputDate.split("-");
 		String date1 = temp.toString();
-		System.out.println(date1);
+		
 		String resultString = "You are ";
 		int sum = 0;
 	    LocalDate todayDate = LocalDate.now();
